feffc9fbf88436307142887002ea9fa176ea1a2b
pedrohenrique096 <moreiraspedro96@gmail.com>
2023-10-02 13:29:07 -0300

    Criação da interação com Js

---
+++ assets/js/script.js
@@ -0,0 +1,80 @@
+
+/*
+por tag: getElementByTagName()
+por Id: getElementById()
+por Nome: geteElementsByName()
+por Classe: geteElementsByClassName()Seletor: querySelector()
+por 
+*/
+
+let nome = window.document.getElementById('nome')
+let email = document.querySelector('#email')
+let assunto = document.querySelector('#assunto')
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
+let mapa = document.querySelector('#mapa')   
+
+
+nome.style.width = '100%'
+email.style.width = '100%'
+
+function validaNome() {
+    let txt = document.querySelector('#txtNome')
+    if (nome.value.length < 3) {
+        txt.innerHTML = 'Nome inválido'
+        txt.style.color = 'red'
+
+    } else {
+        txt.innerHTML = 'Nome Válido'
+        txt.style.color = 'green'
+        nomeOk = true
+    }
+}
+
+function validaEmail() {
+    let txtEmail = document.querySelector('#txtEmail')
+    if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1 ) {
+        txtEmail.innerHTML = 'Email inválido'
+        txtEmail.style.color = 'red'    
+    } else {
+        txtEmail.innerHTML = 'Email válido'
+        txtEmail.style.color = 'green'
+        emailOk = true
+    }
+    
+}
+
+function validaAssunto() {
+    let txtAssunto = document.querySelector ('#txtAssunto')
+
+    if (assunto.value.length > 100) {
+    txtAssunto.innerHTML = 'Utilize apenas 100 caractéres!' 
+    txtAssunto.style.color = 'red'    
+    txtAssunto.style.display = 'block'
+   
+    } else {
+        txtAssunto.style.display = 'none'
+        assuntoOk = true
+    }
+}
+
+function enviar() {
+    if (nomeOk == true && emailOk == true && assuntoOk == true) {
+        alert ('Formulário enviado com sucesso!')  
+    } else {
+        alert ('Preencha o formulário corretamente antes de enviar...')
+    }
+    
+}
+
+function mapaZoom(params) {
+ mapa.style.width = '800px'   
+ mapa.style.height = '600px'   
+}
+
+function mapaNormal(params) {
+    mapa.style.width = '400px'   
+ mapa.style.height = '250px' 
+}
+
